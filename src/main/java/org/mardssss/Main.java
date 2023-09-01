fb643b59220249a14567b5e66363fb2b1a37c11c
BuildTools <unconfigured@null.spigotmc.org>
2023-09-01 20:20:15 +0200

    commit

--- src/main/java/org/mardssss/Main.java
+++ src/main/java/org/mardssss/Main.java
@@ -1,5 +1,20 @@
-package org.mardssss;public class Main {
+package org.mardssss;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkFac();
+}
+public static void checkFac(){
+    int i,fact=1;
+    System.out.println("Write a number to check is factorial!");
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt(); // number to calculate the factorial number
+    for(i=1;i<=number;i++){
+        fact=fact*i;
     }
+    System.out.println("Factorial of "+number+" is: "+fact);
+    checkFac();
+}
 }
